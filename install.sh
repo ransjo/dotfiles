362236a7b9ee3ef232a673130b7b5bf1935d8cd2
Jonas Ransjö <ransjo@gmail.com>
2016-04-11 13:59:56 +0200

    Added tmux configuration

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
 [[ -d ~/.config ]] || mkdir ~/.config
+[[ -d ~/.xmonad ]] || mkdir ~/.xmonad
 [[ -d ~/.oh-my-zsh ]] || git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[[ -d ~/.tmux/plugins/tpm ]] || git clone git://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-#for d in `ls .`;
-#do
-#    ( stow -R $d )
-#done
+for d in `ls .`;
+do
+    ( stow -R $d )
+done
